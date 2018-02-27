c73fc35c757b46b3da808307c244142a198c32fb
Joshua Warren <WarrenJB86@live.com>
2018-02-26 23:04:17 -0800

    Finished the first objective
    After a huge headache, I realized that it's better to convert all of my input arrays into arrays of 25, with leading 0's.  Then, I add from there into a new array.

--- ProgrammingAssignment03/src/SumBigInts.java
+++ ProgrammingAssignment03/src/SumBigInts.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 public class SumBigInts {
     public static final int MAX_DIGITS = 25;
+
     
     /*
      * For this assignment:
@@ -26,24 +27,21 @@ public class SumBigInts {
         //Scanner input = new Scanner(new File("sum.txt"));
         //processFile(input);
         
-        int[] array1 = {1, 2, 3, 4, 3, 2, 1, 4, 3, 2};
-        int[] array2 = {9, 9, 6, 5, 4, 3, 2, 1, 3, 2};
+        int[] array1 = {1, 2, 3, 4, 3, 2, 1, 4, 5};
+        int[] array2 = {9, 9, 6, 5, 4, 3, 2, 1};
         
-        int[] array3 = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
-        int[] array4 = {4, 3, 2, 4, 3, 2};
+
         
         int[] arrayTest = new int[MAX_DIGITS];
         int[] arrayTest1 = new int[MAX_DIGITS];
-        
-        for(int i = 0; i < arrayTest.length; i++) {
-        	arrayTest[i] = addInt(array1, array2)[i];
-        }
-        
+           
+        System.out.print(Arrays.toString(addInt(array1, array2)));
+     /*
         for(int i = 0; i < arrayTest.length; i++) {
         	System.out.print(arrayTest[i] + " ");
         }
         
-        /*
+        
         for(int i = 0; i < arrayTest1.length; i++) {
         	arrayTest1[i] = addInt(array3, array4)[i];
         }
@@ -53,38 +51,47 @@ public class SumBigInts {
         }
         */
         
+        //System.out.print(Arrays.toString(convertArray(array1)));
+        
     } 
 
 // more methods follow for i/o and add and .....
+    
+    public static int[] convertArray(int[] inputArray) {
+    	
+    	int numZero = MAX_DIGITS - inputArray.length;
+    	int[] newArray = new int[MAX_DIGITS];
+    	
+    	for(int i = inputArray.length - 1; i >= 0; i--) {
+    		newArray[i + numZero] = inputArray[i];
+    	}
+    	return newArray;
+    }
     
     
     public static int[] addInt(int[] array1, int[] array2) {
     	
     	int colValue = 0;
-    	int rangeVal = 0;
-    	int arrayMod = 0;
+    	int carryVal = 0;
+    	
+    	array1 = convertArray(array1);
+    	array2 = convertArray(array2);
     	
     	int[] result = new int[MAX_DIGITS];
     	
-    	if(array1.length == array2.length) {
-    		arrayMod = result.length - array1.length + 1; // = 17?
-    	} else {
-    		//arrayMod = (array1.length + array2.length) - result.length;
+    	for(int i = result.length - 1; i >= 0; i--) {
+    		colValue = array1[i] + array2[i] + carryVal;
+    		if(colValue >= 10) {
+    			carryVal = colValue / 10;
+    			result[i] = colValue % 10;
+    			} else {
+    				result[i] = colValue;
+    				carryVal = 0;
+    				}	
     	}
     	
-    	for(int i = result.length - 16; i >= 0; i--) {
-    		colValue = array1[i] + array2[i] + rangeVal;    		
-    		if(colValue >= 10) {
-    			rangeVal = colValue / 10;
-    			result[i + 1] = colValue % 10;
-    			
-    			} else {
-    				result[i + 1] = colValue;
-    				rangeVal = 0;
-    			}
-    		}
-    	result[0] = rangeVal;
-    	return result;
+    	result[0] = carryVal;
+    	return result;  
     }
     
 
