43990736436457853d522473e45a018db2bf5a46
Joshua Warren <WarrenJB86@live.com>
2018-03-07 21:52:55 -0800

    Programming Assignment 4
    Got the majority of this assignment done.  Need addDays, addWeeks, and daysTo methods.
    
    Got started on addDays.

---
+++ ProgrammingAssignment04/src/Client.java
@@ -0,0 +1,16 @@
+
+public class Client {
+	
+	public static void main(String[] args) {
+		
+		
+		Date date1 = new Date(1986, 11, 14);
+		Date date2 = new Date(2001, 1, 1);
+		
+		System.out.println(date1.toString());
+		System.out.println(date2.isLeapYear());
+
+		
+	}
+
+}
---
+++ ProgrammingAssignment04/src/Date.java
@@ -0,0 +1,156 @@
+/*
+ * Programming Assignment 4
+ */
+public class Date {
+	
+	private int year;
+	private int month;
+	private int day;
+	
+	/*
+	 * Constructs a date with an int year, int month
+	 * and an int day.
+	 */
+	public Date(int year, int month, int day) {
+		if (day <= 0 || day > 31) {
+			String s = "Day cannot be less than or equal to 0 or greater than 31.";
+            throw new IllegalArgumentException(s);
+        } else if(month <= 0 || month > 12) {
+        	String s = "Month cannot be less than or equal to 0 or greater than 12.";
+        	throw new IllegalArgumentException(s);
+        }
+		this.year = 1970;
+		this.month = 1;
+		this.day = 1;
+	}
+
+	/*
+	 * Getter method for field year.
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/*
+	 * Setter method for field year.
+	 */
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	/*
+	 * Getter method for field month.
+	 */
+	public int getMonth() {
+		return month;
+	}
+
+	/*
+	 * Setter method for field month.
+	 */
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	/*
+	 * Getter method for field day.
+	 */
+	public int getDay() {
+		return day;
+	}
+
+	/*
+	 * Setter method for field day.
+	 */
+	public void setDay(int day) {
+		this.day = day;
+	}
+	
+	/*
+	 * Moves this Date object forward in time ********
+	 * by the given number of days.
+	 */
+	public void addDays(int days) {
+
+		Date date1 = new Date(year, month, day);
+		int monthInDays = date1.daysInMonth(month);
+		int totalDays = date1.getDay() + days;
+		
+		if(totalDays > monthInDays) {
+			month++;
+		}
+		
+		
+		
+		
+		
+		System.out.println(daysInMonth);
+		
+	}
+	
+	/*
+	 * Moves this Date object forward in time by ********
+	 * the given number of 7-day weeks.
+	 */
+	public void addWeeks(int weeks) {
+		
+	}
+	
+	/*
+	 * Returns the number of days that this Date must be ********
+	 * adjusted to make it equal to the given other Date.
+	 */
+	public int daysTo(Date other) {
+		return day;
+	}
+	
+	/*
+	 * Returns true if the year of this date is a ********
+	 * leap year.
+	 */
+	public boolean isLeapYear() {
+		return ((year %4 == 0) && (year % 100 !=0) || (year % 400 == 0));
+	}
+	
+	/*
+	 * Returns a String representation of this date in 
+	 * year/month/day order.
+	 */
+	public String toString() {
+		return "" + year + "/" + month + "/" + day;
+	}
+	
+	/*
+	 * Helper method that gets the total days in a month.
+	 */
+	public int daysInMonth(int month) {
+		if(month == 1) {
+			return 31;
+		}else if(month == 3) {
+			return 31;
+		}else if(month == 4) {
+			return 30;
+		}else if (month == 5) {
+			return 31;
+		}else if (month == 6) {
+			return 30;
+		}else if (month == 7) {
+			return 31;
+		}else if (month == 8) {
+			return 31;
+		}else if (month == 9) {
+			return 30;
+		}else if (month == 10) {
+			return 31;
+		}else if (month == 11) {
+			return 30;
+		}else if (month == 12) {
+			return 31;
+		}else {
+			return 28;
+		}
+	}
+	
+	
+
+}
